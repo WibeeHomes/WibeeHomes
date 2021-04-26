a66d2dca10e6725e7c18fa5fbe82efdd2c6b11a9
jun991203 <jun991203@naver.com>
2021-04-26 14:24:30 +0900

    activity setting

---
+++ app/src/main/java/app/wibeehomes/HomeDetailActivity.java
@@ -0,0 +1,14 @@
+package app.wibeehomes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_detail);
+    }
+}
---
+++ app/src/main/java/app/wibeehomes/PublicHousingActivity.java
@@ -0,0 +1,14 @@
+package app.wibeehomes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PublicHousingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public_housing);
+    }
+}
