7042aa9f637e057a5299003f4d7821ab937d8df9
Asus <kluiop1@naver.com>
2021-04-29 19:59:23 +0900

    도시 코드 업데이트

---
+++ app/src/main/java/app/wibeehomes/CityCode.java
@@ -0,0 +1,10 @@
+package app.wibeehomes;
+
+public class CityCode {
+    private String cityName;
+    private int cityCode;
+    public CityCode(String cityName, int cityCode){
+        this.cityCode = cityCode;
+        this.cityName = cityName;
+    }
+}
