beeef7014bfa18add794c6f1cd9ddca79ede8b65
Asus <kluiop1@naver.com>
2021-04-30 14:52:01 +0900

    카테고리 api 업데이트

--- app/src/main/java/app/wibeehomes/Map/KakaoMapAPI.java
+++ app/src/main/java/app/wibeehomes/Map/KakaoMapAPI.java
@@ -9,13 +9,22 @@ import android.view.ViewGroup;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
+import java.io.IOException;
+
+import app.wibeehomes.Place;
 import app.wibeehomes.R;
+import app.wibeehomes.SurrFacilities;
 
 public class KakaoMapAPI {
     private MapView mapView;
     private ViewGroup mapViewContainer;
+    private SurrFacilities surrFacilities;
+    private Place centerLoc;
 
-    public KakaoMapAPI(Activity activity, ViewGroup viewGroup, double x, double y){
+    public KakaoMapAPI(Activity activity, ViewGroup viewGroup,Place centerLoc,double x, double y) throws IOException {
+        this.centerLoc = centerLoc;
+        surrFacilities = new SurrFacilities(centerLoc);
+
         mapView = new MapView(activity);
         mapViewContainer = viewGroup;
         mapViewContainer.addView(mapView);
@@ -29,6 +38,10 @@ public class KakaoMapAPI {
 
     public void changeLoc(double x, double y){
         mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(x, y), true);
+    }
+
+    public void changeMarker(){
+
     }
 
     public MapView getMapView() {
