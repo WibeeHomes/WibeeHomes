6e51ad532736dca6be4f6e6b0dcf30477c30a5ce
Asus <kluiop1@naver.com>
2021-04-30 16:47:53 +0900

    우리은행 API 업뎃

--- app/src/main/java/app/wibeehomes/WooriBankAPI/SendAuthentication.java
+++ app/src/main/java/app/wibeehomes/WooriBankAPI/SendAuthentication.java
@@ -5,36 +5,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class SendAuthentication {
-    StringBuilder urlBuilder = new StringBuilder("/oai/wb/v1/login/getCellCerti");
-    URL url = new URL(urlBuilder.toString());
-    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-    String str="{ " +
-            "\"dataHeader\" :{"+
-            "\"UTZPE_CNCT_IPAD\":\"\" ," +
-            "\"UTZPE_CNCT_TEL_NO_TXT\": \"\","+
-            "\"UTZPE_CNCT_MCHR_IDF_SRNO\": \"\","+
-            "\"UTZ_MCHR_OS_DSCD\": \"\","+
-            "\"UTZ_MCHR_OS_VER_NM\": \"\","+
-            "\"UTZ_MCHR_MDL_NM\": \"\","+
-            "\"UTZ_MCHR_APP_VER_NM\": \"\""+
-            "},"+
-            "\"dataBody\": {"+
-            "\"COMC_DIS\": \"1\","+
-            "\"HP_NO\": \"01012345678\","+
-            "\"HP_CRTF_AGR_YN\": \"Y\","+
-            "\"FNM\": \"홍길동\","+
-            "\"RRNO_BFNB\": \"930216\","+
-            "\"ENCY_RRNO_LSNM\": \"1234567\" } }";
+    public static void sendCellAuthentication(int comcDis, String phoneNum,
+                                              String aGRYN,String name,
+                                              String frontBornNum,String backBornNum) throws IOException {
+        StringBuilder urlBuilder = new StringBuilder("/oai/wb/v1/login/getCellCerti");
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        String str="{ " +
+                "\"dataHeader\" :{"+
+                "\"UTZPE_CNCT_IPAD\":\"\" ," +
+                "\"UTZPE_CNCT_TEL_NO_TXT\": \"\","+
+                "\"UTZPE_CNCT_MCHR_IDF_SRNO\": \"\","+
+                "\"UTZ_MCHR_OS_DSCD\": \"\","+
+                "\"UTZ_MCHR_OS_VER_NM\": \"\","+
+                "\"UTZ_MCHR_MDL_NM\": \"\","+
+                "\"UTZ_MCHR_APP_VER_NM\": \"\""+
+                "},"+
+                "\"dataBody\": {"+
+                "\"COMC_DIS\": \""+Integer.toString(comcDis)+"\","+
+                "\"HP_NO\": \""+phoneNum+"\","+
+                "\"HP_CRTF_AGR_YN\": \""+aGRYN+"\","+
+                "\"FNM\": \""+name+"\","+
+                "\"RRNO_BFNB\": \""+frontBornNum+"\","+
+                "\"ENCY_RRNO_LSNM\": \""+backBornNum+"\" } }";
 
-    byte [] body = str.getBytes();
-
-    public SendAuthentication() throws IOException {
+        byte [] body = str.getBytes();
         conn.setRequestMethod("POST");
-        conn.setRequestProperty("appkey", "");
+        conn.setRequestProperty("appkey", "l7xxcYoENLGuojyu3d97r4IM2jCuDNEHnS7P");
         conn.setFixedLengthStreamingMode(body.length);
         conn.setDoOutput(true);
 
