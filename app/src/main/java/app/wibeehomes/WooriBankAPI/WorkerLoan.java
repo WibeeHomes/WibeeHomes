c648e719ea0693897c0e298eb3d2ad9cd58055f5
Asus <kluiop1@naver.com>
2021-05-03 07:03:28 +0900

    중간 정산

--- app/src/main/java/app/wibeehomes/WooriBankAPI/WorkerLoan.java
+++ app/src/main/java/app/wibeehomes/WooriBankAPI/WorkerLoan.java
@@ -3,12 +3,12 @@ package app.wibeehomes.WooriBankAPI;
 public class WorkerLoan {
     private String ENCY; // 여신 결정 금리
     private String name; //  적용 가산 금리
-    private int RQAM; // 승인 금액
-    private int BZPE;//처리상태코드
-    private int ENCN;//여신승인번호
+    private String RQAM; // 승인 금액
+    private String BZPE;//처리상태코드
+    private String ENCN;//여신승인번호
 
-    public WorkerLoan(String ENCY, String name, int RQAM,
-                      int BZPE,int ENCN){
+    public WorkerLoan(String ENCY, String name, String RQAM,
+                      String BZPE,String ENCN){
         this.BZPE =BZPE;
         this.ENCN=ENCN;
         this.ENCY = ENCY;
--- app/src/main/java/app/wibeehomes/WooriBankAPI/bisangLoan.java
+++ app/src/main/java/app/wibeehomes/WooriBankAPI/bisangLoan.java
@@ -1,15 +1,15 @@
 package app.wibeehomes.WooriBankAPI;
 
 public class bisangLoan {
-    private int LONDCS; // 여신 결정 금리
-    private int APLIR; // 적용 가산금리
-    private int TGTCUS; // 대상 고객 우대 금리
-    private int WooriPBOK; // 우리통장보유우대 금리
-    private int APVAM; // 승인 금액
+    private String LONDCS; // 여신 결정 금리
+    private String APLIR; // 적용 가산금리
+    private String TGTCUS; // 대상 고객 우대 금리
+    private String WooriPBOK; // 우리통장보유우대 금리
+    private String APVAM; // 승인 금액
     private String PRC;// 처리 상태 코드
 
-    public bisangLoan(int LONDCS, int APLIR,int TGTCUS,
-                      int WooriPBOK,int APVAM,String PRC){
+    public bisangLoan(String LONDCS, String APLIR,String TGTCUS,
+                      String WooriPBOK,String APVAM,String PRC){
         this.APLIR =APLIR;
         this.APVAM = APVAM;
         this.LONDCS = LONDCS;
